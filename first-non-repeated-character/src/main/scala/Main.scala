3f03fad42357c58d738e2f5791166687db59a67e
Alexander Sidorenko <aleks.sidorenko@gmail.com>
2015-11-09 19:43:05 +0200

    first-non-repeated-character

---
+++ first-non-repeated-character/src/main/scala/Main.scala
@@ -0,0 +1,10 @@
+object Main extends App {
+  val source = scala.io.Source.fromFile(args(0))
+  val lines = source.getLines.filter(_.length > 0)
+  for (line <- lines) {
+    line.find( x => line.indexOf(x) == line.lastIndexOf(x)) match  {
+      case Some(x) => println(x)
+    }
+
+  }
+}
