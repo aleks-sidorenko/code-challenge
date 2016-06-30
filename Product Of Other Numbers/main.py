0b39b1072daa1759f357cb82af417de8377f7963
Alexander Sidorenko <aleks.sidorenko@gmail.com>
2016-06-30 21:22:54 +0300

    Added Product of other numbers

---
+++ Product Of Other Numbers/main.py
@@ -0,0 +1,13 @@
+from functools import reduce
+import sys
+
+def product_of_other_numbers(array):
+    total = reduce(lambda x, y: x*y, array)
+    return list(map(lambda x: int(total/x), array))
+
+
+with open(sys.argv[1], 'r') as test_cases:
+    for test in test_cases:
+        array = list(map(int, test.strip().split(";")))
+        result = product_of_other_numbers(array)
+        print(result)
