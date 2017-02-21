f88354364629f2ae54b64a59e0bf179c3f744432
Alexander Sidorenko <asidorenko@viewster.com>
2017-02-21 19:16:30 +0200

    [Binary Search: Ice Cream Parlor] Optimized

--- ctci-ice-cream-parlor/main.scala
+++ ctci-ice-cream-parlor/main.scala
@@ -3,68 +3,25 @@ object Solution {
     
         
     case class Trip(m: Int, flavors: IndexedSeq[Int]) {
-        // bit string which define match like '001100'
-        type Bits = String
         
-        private lazy val optimizedFlavors = flavors.zipWithIndex.filter(_._1 < m).sortBy(_._1)
+        lazy val optimized = flavors.zipWithIndex.filter(_._1 < m)
                     
         def matched(): (Int, Int) = {
-            val initial = "0" * optimizedFlavors.length
             
-            def matched(total: Int, level: Int, mch: Bits): Option[Bits] = {
-                                
-                val numBits = bits(mch)
-                                                   
-                if (total == 0) {
-                    if (numBits == 2) return Some(mch)
-                    else return None                    
-                }
+            val valid = optimized.groupBy(_._1).map { case(k, v) => (k, v.map(_._2))}
+            val discounted = optimized.map(m - _._1).filter(v => m - v != v).toSet
+            val discounted2 = optimized.map(m - _._1).filter(v => m - v == v).toSet
+            val intersection = valid.keySet & discounted
+            val intersection2 = valid.keySet & discounted2
                 
-                    
-                if (level > optimizedFlavors.length || total < 0) {
-                    return None
-                }
-                                
-                val i = level - 1
-                val cur = optimizedFlavors(i)._1
-                val newMch = setBit(mch, i)
-                 
-                val right = matched(total - cur, level + 1, newMch)
-                if (right.isDefined) {
-                    return right
-                }
-                
-                val left = matched(total, level + 1, mch)
-                if (left.isDefined) {
-                    return left
-                }
-                
-                return None
-                
-            }
+            def indexes = (set: Set[Int]) => set.flatMap(valid(_)).map(_ + 1).toList.sorted
+            val inter = if (intersection.isEmpty) intersection2 else intersection
+            val ret = indexes(inter)
+                        
+            (ret(0), ret(1))
             
-            val res = matched(m, 1, initial)
-            res match {
-                case Some(ret) => bitsToTuple(ret)
-                case _ => throw new IllegalArgumentException()
-            }
         }
-        
-        private def bitsToTuple(mch: Bits): (Int, Int) = {
-            
-            val indexes = mch.zipWithIndex.filter { case (b, i) => b == '1' } map { case (_, i) => i }
-            
-            val ids = indexes.map({ i => optimizedFlavors(i)._2 + 1 }).sorted
-            (ids(0), ids(1))
-        }
-        
-        private def bits(mch: Bits): Int = mch.count(_ == '1')
-            
-        private def setBit(mch: Bits, i: Int): Bits = {
-            val str = new StringBuilder(mch)
-            str(i) = '1'
-            str.toString.asInstanceOf[Bits]
-        }
+                
     }
         
     def readInput(): List[Trip] = {
@@ -80,7 +37,7 @@ object Solution {
                a(a_i) = sc.nextInt()
             }
             trips(a0) = Trip(m, a)
-            a0+=1;
+            a0+=1
         }
         trips.toList
     }
