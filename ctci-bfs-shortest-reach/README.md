22a8c18a440908370b4c4bce83a4c8cb3a87c47f
Alexander Sidorenko <aleks.sidorenko@gmail.com>
2017-02-23 03:30:20 +0200

    BFS: Shortest Reach in a Graph] Started

---
+++ ctci-bfs-shortest-reach/README.md
@@ -0,0 +1,1 @@
+[BFS: Shortest Reach in a Graph](https://www.hackerrank.com/challenges/ctci-bfs-shortest-reach)
---
+++ ctci-bfs-shortest-reach/main.scala
@@ -0,0 +1,70 @@
+import scala.collection._
+    
+object Solution {
+
+    case class Graph(nodeCount: Int, edgeCount: Int, root: GraphNode) {
+        private final val Initial = -1
+        private final val Ratio = 6
+        
+        private val dist = Array.fill(nodeCount)(Initial)
+        private val visited = new mutable.HashSet[Int]()
+        
+        def distances: Seq[Int] = {
+            
+            visit(root, 0)
+            val result = dist.take(root.index) ++ dist.drop(root.index + 1)
+            result.map {d => if (d != Initial) d * Ratio else d }
+        }
+        
+
+        private def process(node: GraphNode, level: Int): Unit = {
+            if (dist(node.index) == Initial) dist(node.index) = level
+
+            dist(node.index) = math.min(level, dist(node.index))
+        }
+        
+        private def visit(node: GraphNode, level: Int): Unit = {
+            println(s"nodeCount: $nodeCount, edgeCount: $edgeCount, visiting node: ${node.id}, level: $level, dist: ${dist(node.index)}")
+            if (dist(node.index) != Initial && dist(node.index) < level) return
+
+            process(node, level)
+
+            node.links.foreach {l => process(l, level + 1)}
+            node.links.foreach {l => visit(l, level + 1)}
+        }
+
+    }
+    
+    case class GraphNode(id: Int, links: mutable.Buffer[GraphNode] = new mutable.ArrayBuffer[GraphNode]) {
+        val index = id - 1
+    }
+        
+    def readInput(): Seq[Graph] = {
+        val sc = new java.util.Scanner(System.in)
+        val q = sc.nextInt()
+        val graphs = new Array[Graph](q)
+        for (i <- 1 to q) {
+            val n = sc.nextInt()
+            val m = sc.nextInt()
+            val nodes = 1 to n map { j => j -> GraphNode(j) } toMap
+            
+            for (k <- 1 to m) {
+                val (from, to) = sc.nextInt() -> sc.nextInt()
+                val fromNode = nodes(from)
+                val toNode = nodes(to)
+                fromNode.links += toNode
+                toNode.links += fromNode
+            }
+            val root = sc.nextInt()
+            val rootNode = nodes(root)
+            
+            graphs(i - 1) = Graph(nodeCount = n, edgeCount = m, root = rootNode)
+        }
+        graphs.toSeq
+    }
+    
+    def main(args: Array[String]) {
+        val graphs = readInput()
+        graphs.foreach(g => println(g.distances.mkString(" ")))
+    }
+}
