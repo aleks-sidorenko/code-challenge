293d7a00a0c72e91be9ccf9c9fcb0d9c95f7b42f
Alexander Sidorenko <asidorenko@viewster.com>
2017-03-27 19:16:44 +0300

    [Password Cracker] Started

---
+++ password-cracker/README.md
@@ -0,0 +1,1 @@
+[Password Cracker](https://www.hackerrank.com/challenges/password-cracker)
---
+++ password-cracker/main.scala
@@ -0,0 +1,60 @@
+object Solution {
+    
+    final case class Case(passwords: List[String], attempt: String) {
+        def check(): List[String] = {
+            check(attempt, passwords) match {
+                case None => Nil
+                case Some(l) => l
+            }
+        }
+        
+        private def check(str: String, words: List[String]): Option[List[String]] = {
+            if (str.isEmpty) return Some(Nil)
+            
+            val matched = words.filter(str.startsWith(_))
+            if (matched.isEmpty) return None
+                
+            val res = matched.flatMap { m => 
+                val rest = str.slice(m.length, str.length - m.length)
+                check(rest, words) match {
+                    case Some(list) => rest :: list
+                    case None => Nil
+                }
+            }
+            
+            res match {
+                case Nil => None
+                case l: List[String] => Some(l)
+            }
+                
+        }
+    }
+    
+    def readInput(): List[Case] = {
+        val sc = new java.util.Scanner(System.in)    
+        val t = sc.nextInt
+        val cases = new Array[Case](t)
+        for (i <- 0 until t) {
+            val n = sc.nextInt
+            val passwords = new Array[String](n)
+            for (j <- 0 until n) {
+                passwords(j) = sc.next()
+            }
+            val attempt = sc.next()
+            cases(i) = Case(passwords.toList, attempt)    
+        }
+        cases.toList
+        
+    }
+    
+    def main(args: Array[String]) {
+        val cases = readInput()
+        cases.foreach { c => 
+            println(c.check match {
+                case Nil => "WRONG PASSWORD"
+                case l: List[String] => l.mkString(" ")
+            })
+        }
+        
+    }
+}
