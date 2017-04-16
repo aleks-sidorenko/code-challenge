808537bf44a7c7a639490494317152d1aaa94d54
Alexander Sidorenko <aleks.sidorenko@gmail.com>
2017-04-17 00:02:01 +0300

    [Weighted meeting room] Solved

--- careercup/weighted-meeting-room-5748568523538432/main.scala
+++ careercup/weighted-meeting-room-5748568523538432/main.scala
@@ -1,21 +1,59 @@
 object Solution {
 
+  import scala.collection.mutable.{Buffer, ListBuffer}
 
-  case class Meeting(start: Long, end: Long, weight: Int)
+  case class Meeting(start: Long, end: Long, weight: Int) extends Ordered[Meeting] {
+    def isOvelappedBy(meeting: Meeting) = {
+      val (min, max) = if (this <= meeting) (this, meeting) else (meeting, this)
+      (min.end > max.start && min.end < max.end) || (min.end >= max.end)
+    }
+
+    override def compare(that: Meeting) = Meeting.ordering.compare(this, that)
+  }
+
+  object Meeting {
+    def parse(sc: java.util.Scanner) = Meeting(sc.next().toLong, sc.next().toLong, sc.nextInt())
+
+    implicit val ordering: Ordering[Meeting] = Ordering.by(_.start)
+  }
 
   def readInput(): List[Meeting] = {
 
     val sc = new java.util.Scanner(System.in)
     val n = sc.nextInt
     val meetings = collection.mutable.ListBuffer.empty[Meeting]
-    for (i <- 0 until n) meetings += Meeting(sc.next().toLong, sc.next().toLong, sc.nextInt())
+    for (i <- 0 until n) meetings += Meeting.parse(sc)
     meetings.toList
   }
 
-  
+  def schedule(meetings: List[Meeting]): Int = {
+    val sorted = meetings.sorted
+    
+
+    def partitionOverlappings(meeting: Meeting, meetings: List[Meeting]): (List[Meeting], List[Meeting]) = {
+      (meetings.filter(meeting.isOvelappedBy(_)), meetings.filter(!meeting.isOvelappedBy(_)))
+    }
+
+    def scheduleHelper(meetings: List[Meeting], weight: Int): Int = {
+      meetings match {
+        case meeting::rest => {
+          val (overlappings, notOverlappings) = partitionOverlappings(meeting, meetings)
+          math.max(
+            scheduleHelper(notOverlappings, weight + meeting.weight),
+            overlappings.map(o => scheduleHelper(rest, weight)).max
+            )
+        }
+
+        case _ => weight
+      }
+    }
+
+    scheduleHelper(meetings, 0)
+
+  }
+
   def main(args: Array[String]) {
     val meetings = readInput
-
-    meetings.foreach(m => println(m))
+    println(schedule(meetings))
   }
 }
