79fdb155c745e6a6be6c02308eebd790a5e418f5
Alexander Sidorenko <aleks.sidorenko@gmail.com>
2017-05-01 20:48:47 +0300

    [Dijkstra: Shortest Reach 2] Improved

--- hackerrank/dijkstrashortreach/main.scala
+++ hackerrank/dijkstrashortreach/main.scala
@@ -10,70 +10,67 @@ object Solution {
   case class Edge(from: NodeId, to: NodeId, distance: Distance)
   
   case class Graph(nodes: List[Node], edges: List[Edge]) {
-    
-
     case class Connection(to: NodeId, distance: Distance)
     type DistMap = mutable.Map[NodeId, Distance]
     type VisitMap = mutable.Map[NodeId, Boolean]
 
-    private final val initialDistance = Distance.MaxValue
+    private final val initialDistance = Int.MaxValue
 
     private val nodeMap = nodes.map(n => n.id -> n).toMap
     private val connectionMap = nodes.map(n => n.id -> connections(n.id)).toMap
 
 
     def distances(start: NodeId): List[Distance] = {
-      val dist = dist(start, () => initialDistance)
-      val toVisit = toVisit()
+      val dist = toDist(start, initialDistance)
+      val visit = toVisit()
     
-      val nodesToVisit = { nodeId: NodeId =>
-        connectionMap(nodeId).map(_.to).filter(n => !visited(n)).toSet.toList
-      }
-
       val processNode = { nodeId: NodeId =>
         var d = dist(nodeId)
-        if (d == initialDistance) { d = 0; dist(nodeId) = d }
         val connections = connectionMap(nodeId)
         for (c <- connections) {
-          if (dist(c.to) == initialDistance || (d + c.distance <  dist(c.to))) dist(c.to) = d + c.distance
+          if (visit(c.to) && d + c.distance < dist(c.to)) dist(c.to) = d + c.distance
         }
       }
 
-      val nextNode = {
-      
+      val nextNode = () => {
+        val nodeId = visit.minBy(dist(_))
+        if (dist(nodeId) < initialDistance) {
+          visit -= nodeId
+          Some(nodeId)
+        }
+        else None
       }
 
-      while (!toVisit.isEmpty) {
-        val nodeId = queue.dequeue
-        
-        visited(nodeId) = true
-        processNode(nodeId)
-        queue ++= nodesToVisit(nodeId)
+      val distNoStart = () => dist.filter(_._1 != start).toSeq.sortBy(_._1).map(d => if (d._2 == initialDistance) -1 else d._2).toList
+
+      var nodeId = nextNode()
+      while (!visit.isEmpty && nodeId.isDefined) {
+        processNode(nodeId.get)
+        nodeId = nextNode()
       }
 
       // take all except start
-      dist.filter(_._1 != start).toSeq.sortBy(_._1).map(_._2).toList
+      distNoStart()
+      
     }
 
     private def connections(nodeId: NodeId): List[Connection] = {
-      val c = edges.filter(e => e.from == nodeId).map(e => Connection(e.to, e.distance)) ++ 
+      edges.filter(e => e.from == nodeId).map(e => Connection(e.to, e.distance)) ++ 
         edges.filter(e => e.to == nodeId).map(e => Connection(e.from, e.distance))
-      c.sortBy(_.distance)
     }
 
-    private def dist[V](start: NodeId, default: () => V): mutable.Map[NodeId, V] = {
-      val map = mutable.Map.empty[NodeId, V]
-      nodes.foreach(n => map(n.id) = default())
+    private def toDist(start: NodeId, default: Distance): DistMap = {
+      val map = mutable.Map.empty[NodeId, Int]
+      nodes.foreach(n => map(n.id) = default)
       map(start) = 0
       map
     }
 
     private def toVisit() = {
-      val set = mutable.Set.empty[NodeId]
+      val set = mutable.Set[NodeId]()
       nodes.foreach(n => set += n.id)
       set
     }
-
 
   }
 
