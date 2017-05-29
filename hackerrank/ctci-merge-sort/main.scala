c392cca00a95fa9d3db35ed63a353ba250379f85
Alexander Sidorenko <asidorenko@viewster.com>
2017-05-29 19:15:07 +0300

    [Merge Sort: Counting Inversions] Solved

--- hackerrank/ctci-merge-sort/main.scala
+++ hackerrank/ctci-merge-sort/main.scala
@@ -17,8 +17,52 @@ object Solution {
         
     }
 
-    def inversions(numbers: List[Int]): Int = {
-        numbers.head
+    def inversions(numbers: List[Int]): Long = {
+
+        def merge(array: Array[Int], start: Int, middle: Int, end: Int): Long = {
+
+            val left = array.slice(start, middle)
+            val right = array.slice(middle, end)
+
+            var i, j = 0
+            var inv: Long = 0
+            var k = start
+
+            while (i < left.length && j < right.length) {
+                if (left(i) <= right(j)) {
+                    array(k) = left(i)
+                    i += 1
+                }
+                else {
+                    array(k) = right(j)
+                    j += 1
+                    inv += (left.length - i)
+                }
+                k += 1
+            }
+            
+            while (i < left.length) {
+                array(k) = left(i)
+                i += 1
+                k += 1
+            }
+
+            while (j < right.length) {
+                array(k) = right(j)
+                j += 1
+                k += 1
+            }
+
+            inv
+        }
+
+        def mergeSort(array: Array[Int], start: Int, end: Int): Long = {
+            if (start + 1 >= end) return 0
+            val middle: Int = (end + start) / 2
+            mergeSort(array, start, middle) + mergeSort(array, middle, end) + merge(array, start, middle, end)
+        }
+        
+        mergeSort(numbers.toArray, 0, numbers.length)
     }
 
     def main(args: Array[String]) {
