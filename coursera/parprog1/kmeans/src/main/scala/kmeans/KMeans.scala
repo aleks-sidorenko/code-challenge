f5dc128f34d21f118d0c3012c720a60e8a927bed
Alexander Sidorenko <aleks.sidorenko@gmail.com>
2017-06-29 23:09:33 +0300

    [Coursera: ParProg1: K-Means] Solved

--- coursera/parprog1/kmeans/src/main/scala/kmeans/KMeans.scala
+++ coursera/parprog1/kmeans/src/main/scala/kmeans/KMeans.scala
@@ -43,7 +43,7 @@ class KMeans {
   }
 
   def classify(points: GenSeq[Point], means: GenSeq[Point]): GenMap[Point, GenSeq[Point]] = {
-    ???
+    means.groupBy(identity).mapValues(_ => Seq()) ++ points.groupBy(findClosest(_, means))
   }
 
   def findAverage(oldMean: Point, points: GenSeq[Point]): Point = if (points.length == 0) oldMean else {
@@ -59,16 +59,20 @@ class KMeans {
   }
 
   def update(classified: GenMap[Point, GenSeq[Point]], oldMeans: GenSeq[Point]): GenSeq[Point] = {
-    ???
+    oldMeans.map(m => findAverage(m, classified(m)))
   }
 
   def converged(eta: Double)(oldMeans: GenSeq[Point], newMeans: GenSeq[Point]): Boolean = {
-    ???
+    oldMeans.zip(newMeans).forall  { case (p1, p2) => p1.squareDistance(p2) <= eta }
   }
 
   @tailrec
   final def kMeans(points: GenSeq[Point], means: GenSeq[Point], eta: Double): GenSeq[Point] = {
-    if (???) kMeans(???, ???, ???) else ??? // your implementation need to be tail recursive
+    val classified = classify(points, means)
+    val oldMeans = means
+    val newMeans = update(classified, means)
+
+    if (!converged(eta)(oldMeans, newMeans)) kMeans(points, newMeans, eta) else means // your implementation need to be tail recursive
   }
 }
 
