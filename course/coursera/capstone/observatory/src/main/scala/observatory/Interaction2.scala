518577076543178ad8aa2d2acb6e5990d5f0dadc
Alexander Sidorenko <aleks.sidorenko@gmail.com>
2017-08-16 23:27:17 +0300

    [Coursera: Scala Capstone: Observatory] 9.38 out of 10.00

--- course/coursera/capstone/observatory/src/main/scala/observatory/Interaction2.scala
+++ course/coursera/capstone/observatory/src/main/scala/observatory/Interaction2.scala
@@ -9,7 +9,34 @@ object Interaction2 {
     * @return The available layers of the application
     */
   def availableLayers: Seq[Layer] = {
-    ???
+    Layer(
+      LayerName.Temperatures,
+      Seq(
+        (60.0, Color(255, 255, 255)),
+        (32.0, Color(255, 0, 0)),
+        (12.0, Color(255, 255, 0)),
+        (0.0, Color(0, 255, 255)),
+        (-15.0, Color(0, 0, 255)),
+        (-27.0, Color(255, 0, 255)),
+        (-50.0, Color(33, 0, 107)),
+        (-60.0, Color(0, 0, 0))
+      ),
+      1975 to 2015
+    ) ::
+      Layer(
+        LayerName.Deviations,
+        Seq(
+          (7.0, Color(0, 0, 0)),
+          (4.0, Color(255, 0, 0)),
+          (2.0, Color(255, 255, 0)),
+          (0.0, Color(255, 255, 255)),
+          (-2.0, Color(0, 255, 255)),
+          (-7.0, Color(0, 0, 255))
+        ),
+        1975 to 2015
+      ) ::
+      Nil
+
   }
 
   /**
@@ -17,51 +44,63 @@ object Interaction2 {
     * @return A signal containing the year bounds corresponding to the selected layer
     */
   def yearBounds(selectedLayer: Signal[Layer]): Signal[Range] = {
-    ???
+    Signal(selectedLayer().bounds)
   }
 
   /**
     * @param selectedLayer The selected layer
-    * @param sliderValue The value of the year slider
+    * @param sliderValue   The value of the year slider
     * @return The value of the selected year, so that it never goes out of the layer bounds.
     *         If the value of `sliderValue` is out of the `selectedLayer` bounds,
     *         this method should return the closest value that is included
     *         in the `selectedLayer` bounds.
     */
   def yearSelection(selectedLayer: Signal[Layer], sliderValue: Signal[Int]): Signal[Int] = {
-    ???
+    val slider = sliderValue()
+    val bounds = yearBounds(selectedLayer)()
+    Signal(slider max bounds.min min bounds.max)
   }
 
   /**
     * @param selectedLayer The selected layer
-    * @param selectedYear The selected year
+    * @param selectedYear  The selected year
     * @return The URL pattern to retrieve tiles
     */
   def layerUrlPattern(selectedLayer: Signal[Layer], selectedYear: Signal[Int]): Signal[String] = {
-    ???
+    val year = selectedYear()
+    val layer = selectedLayer()
+    Signal(s"generated/${layer.layerName.id}/${year}/{z}/{x}/{y}.png")
   }
 
   /**
     * @param selectedLayer The selected layer
-    * @param selectedYear The selected year
+    * @param selectedYear  The selected year
     * @return The caption to show
     */
   def caption(selectedLayer: Signal[Layer], selectedYear: Signal[Int]): Signal[String] = {
-    ???
+
+    val year = selectedYear()
+    val layer = selectedLayer()
+
+    Signal(s"${layer.layerName.id.capitalize} (${year})")
   }
 
 }
 
 sealed abstract class LayerName(val id: String)
+
 object LayerName {
+
   case object Temperatures extends LayerName("temperatures")
+
   case object Deviations extends LayerName("deviations")
+
 }
 
 /**
-  * @param layerName Name of the layer
+  * @param layerName  Name of the layer
   * @param colorScale Color scale used by the layer
-  * @param bounds Minimum and maximum year supported by the layer
+  * @param bounds     Minimum and maximum year supported by the layer
   */
 case class Layer(layerName: LayerName, colorScale: Seq[(Double, Color)], bounds: Range)
 
