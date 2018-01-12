d13a703fe427ec6f479fd72d60d5d615bbf4f647
Aleksandr Sidorenko <aleks.sidorenko@gmail.com>
2018-01-12 17:50:03 +0200

    [Maximum Length Chain of Pairs] Solved

--- challenge/geeksforgeeks/max-length-chain/main.scala
+++ challenge/geeksforgeeks/max-length-chain/main.scala
@@ -1,14 +1,11 @@
 object Solution {
   import scala.collection._
 
-
   type Pair = (Int, Int)
-
 
   def readInput(): List[List[Pair]] = {
     val sc = new java.util.Scanner(System.in)
-    
-    
+
     val t = sc.nextInt
     val cases = new Array[List[Pair]](t)
     for (i <- 0 until t) {
@@ -19,17 +16,35 @@ object Solution {
       }
 
       cases(i) = pairs.toList
-      
+
     }
 
     cases.toList
-    
+
   }
 
-
   case class Solution(pairs: List[Pair]) {
+
+    // caching
+    val cache = collection.mutable.HashMap.empty[(Int, Int, Int), Int]
+
     final def solve(): Int = {
-      pairs.length
+      step(pairs, 0, Int.MinValue)
+    }
+
+    private def step(pairs: List[Pair], acc: Int, min: Int): Int = {
+
+      cache.getOrElseUpdate(
+        (pairs.length, acc, min),
+        pairs match {
+          case p :: ps =>
+            val (a, b) = p
+            val ignore = step(ps, acc, min)
+            if (a < min) math.max(step(ps, 1, b), ignore)
+            else math.max(step(ps, acc + 1, b), ignore)
+          case _ => acc
+        }
+      )
     }
 
   }
@@ -38,8 +53,7 @@ object Solution {
     val cases = readInput()
 
     cases.foreach { c =>
-      println(s"$c")
       println(Solution(c).solve())
-    }    
+    }
   }
 }
