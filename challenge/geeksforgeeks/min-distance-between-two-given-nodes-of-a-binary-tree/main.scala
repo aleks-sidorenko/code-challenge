6d821b992f74dffec768fa7da616107f157db8eb
Alexander Sidorenko <aleks.sidorenko@gmail.com>
2018-02-21 00:14:47 +0200

    [Min distance between two given nodes of a Binary Tree] Solved

--- challenge/geeksforgeeks/min-distance-between-two-given-nodes-of-a-binary-tree/main.scala
+++ challenge/geeksforgeeks/min-distance-between-two-given-nodes-of-a-binary-tree/main.scala
@@ -1,52 +1,61 @@
 object Solution {
   import scala.collection._
 
-  sealed trait Tree {
-    def left: Tree
-    def right: Tree
+  type Data = Int
+
+  sealed trait Tree[T] {
+    def data: T
+    def left: Option[Tree[T]]
+    def right: Option[Tree[T]]
   }
 
-  case class Node(data: Int, right: Tree, left: Tree) extends Tree
+  case class Node[T](data: T, right: Option[Tree[T]], left: Option[Tree[T]]) extends Tree[T]
 
-  case class Leaf(data: Int) extends Tree {
-    val left = Empty
-    val right = Empty
+  case class Leaf[T](data: T) extends Tree[T] {
+    val left = None
+    val right = None
   }
-
-  case object Empty extends Tree {
-    val left = Empty
-    val right = Empty
-  }
-
-  type Case = (Tree, (Int, Int))
-  type Result = Int
+  
+  type Case = (Tree[Int], (Data, Data))
+  type Result = Int    
+    
 
   def readInput(): List[Case] = {
-    (Node(1, Leaf(2), Leaf(3)) -> (2, 3)) :: (Node(1,
-                                                   Node(2, Leaf(4), Leaf(5)),
-                                                   Leaf(3)) -> (4, 5)) :: Nil
+    val tree1: Tree[Int] = Node(1, Some(Leaf(2)), Some(Leaf(3)))
+    val tree2: Tree[Int] = Node(4, Some(tree1), Some(Leaf(6)))
+    (tree1 -> (2, 3)) :: (tree2 -> (6, 3)) :: (tree2 -> (1, 3)) :: Nil
   }
 
   sealed trait Solution {
     def solve(input: Case): Result
   }
 
+
   class Solution1 extends Solution {
 
     final def solve(input: Case): Result = {
       val (tree, (a, b)) = input
 
-      def go(tree: Tree, acc: Int): Int = {
-        tree match {
-          case Leaf(d) if l == d || d == b => acc + 1
-          case Node(d, l, r)               =>
-          case Empty                       => acc
+      def findPath(tree: Tree[Data], d: Data, path: List[Data]): List[Data] = {
+        if (tree.data == d) d :: path
+        else {
+          tree match {
+            case Leaf(_) => Nil
+            case Node(n, l, r) => 
+              val left = l.map(findPath(_, d, n :: path)).getOrElse(Nil)
+              val right = r.map(findPath(_, d, n :: path)).getOrElse(Nil)
+              if (left.nonEmpty) left else if (right.nonEmpty) right else Nil
+          }
         }
       }
 
-      go(tree, 0)
-    }
+      val pathA = findPath(tree, a, Nil)
+      val pathB = findPath(tree, b, Nil)
+      val all = pathA.toSet | pathB.toSet
+      val diff = pathA.toSet & pathB.toSet
+      (all &~ diff).size
 
+    }
   }
 
   def main(args: Array[String]) = {
