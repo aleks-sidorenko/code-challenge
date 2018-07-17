61b4cfb54cd018591800adc36d3e2ebcfcd4a861
Aleksandr Sidorenko <aleks.sidorenko@gmail.com>
2018-07-17 22:39:03 +0300

    [Left View of Binary Tree] Done

--- challenge/geeksforgeeks/left-view-of-binary-tree/main.scala
+++ challenge/geeksforgeeks/left-view-of-binary-tree/main.scala
@@ -23,23 +23,46 @@ object Solution {
     val tree1: Tree[Int] = Node(1, Some(Leaf(2)), Some(Leaf(3)))
     val tree2: Tree[Int] = Node(
       10,
-      Some(Node(20, 
-            Some(Node(40, Some(Leaf(60)), None)), None)),
-      Some(Leaf(30)))
+      Some(Node(20, Some(Node(40, Some(Leaf(60)), None)), None)),
+      Some(Node(30, Some(Node(50, Some(Node(70, 
+        Some(Leaf(90)), None)), None)), None))
+      )
     tree1 :: tree2 :: Nil
   }
 
   case class Solution(tree: Tree[Data]) {
+    type Level = Int
 
     final def solve(): List[Data] = {
-      Nil
+      def loop(node: Option[Tree[Data]],
+               view: Map[Level, Data],
+               level: Level): Map[Level, Data] = {
+        node match {
+          case Some(Node(d, l, r)) =>
+            view.get(level) match {
+              case Some(_) =>
+                loop(l, view, level + 1) ++ loop(r, view, level + 1)
+              case None =>
+                 loop(r, view + (level -> d), level + 1) ++ loop(l, view + (level -> d), level + 1)
+            }
+          case Some(Leaf(d)) =>
+            view.get(level) match {
+              case Some(_) => view
+              case None    => view + (level -> d)
+            }
+          case None => view
+        }
+      }
+      val res = loop(Some(tree), Map.empty[Level, Data], 0)
+
+      SortedMap(res.toSeq: _*).values.toList
     }
 
   }
 
   def main(args: Array[String]) = {
     val cases = readInput()
-    cases.foreach { c => 
+    cases.foreach { c =>
       println(Solution(c).solve().mkString(" "))
     }
   }
