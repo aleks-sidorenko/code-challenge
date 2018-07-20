0351d9b932d29a0f788e49fde92bb651a277e344
Aleksandr Sidorenko <aleks.sidorenko@gmail.com>
2018-07-20 23:42:58 +0300

    [LRU Cache Implementation] Done

--- challenge/geeksforgeeks/lru-cache-implementation/main.scala
+++ challenge/geeksforgeeks/lru-cache-implementation/main.scala
@@ -20,8 +20,67 @@ object Solution {
 
   }
 
-  case class Solution(numbers: List[Int]) {
+  class Queue[T] {
+    class Node(val value: T,
+               var prev: Option[Item] = None,
+               var next: Option[Item] = None)
 
+    private var front: Option[Item] = None
+    private var rear: Option[Item] = None
+
+    def isEmpty = front.isEmpty && rear.isEmpty
+
+    def update(node: Node) = ???
+
+    def enqueue(v: T) = {
+      val n = new Node(v, None, rear)
+      if (isEmpty) {
+        front = rear = Some(n)
+      } else {
+        rear.prev = Some(n)
+        rear = n
+      }
+    }
+
+    def expire() = {
+      if (isEmpty) {
+        front = rear = None
+      } else {
+        front = front.prev
+        if (front.isEmpty) front = rear
+      }
+    }
+
+  }
+
+  class LruCache[K, T](val limit: Int) {
+    private val queue = new Queue[T]()
+    private val map = new mutable.Map[K, Queue[T]#Node]
+
+    def get(k: K): Option[T] = {
+      val res = map.get(k)
+      res match {
+        case Some(_) =>
+          queue.update(k)
+          res
+        case _ =>
+          res
+      }
+    }
+
+    def put(k: K, v: T): Option[K] = {
+      if (limit < map.size) {
+        val node = queue.enqueue(v)
+        map += (k -> node)
+      } else {
+        val exp = queue.expire
+        map.remove(exp.key)
+      }
+    }
+  }
+
+  case class Solution(numbers: List[Int]) {
+    private val cache = new LruCache[Int]()
     final def solve(): Int = {
       0
     }
