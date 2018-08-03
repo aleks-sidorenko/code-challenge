26d440c7e1e84588dd02a29cf309c9efef37d8a7
Alexander Sidorenko <aleks.sidorenko@gmail.com>
2018-08-03 23:46:33 +0300

    [LRU Cache Implementation] Improved

--- challenge/geeksforgeeks/lru-cache-implementation/main.scala
+++ challenge/geeksforgeeks/lru-cache-implementation/main.scala
@@ -1,97 +1,53 @@
 object Solution {
   import scala.collection._
 
-  def readInput(): List[List[Int]] = {
+  type Limit = Int
+  sealed trait Input
+  case class Put(k: String, v: String) extends Input
+  case class Get(k: String) extends Input
+
+
+  def readInput(): List[(Limit, List[Input])] = {
     val sc = new java.util.Scanner(System.in)
 
     val t = sc.nextInt
-    val cases = new Array[List[Int]](t)
+    val cases = new Array[(Limit, List[Input])](t)
     for (i <- 0 until t) {
+      val l = sc.nextInt
       val n = sc.nextInt
-      val list = new Array[Int](n)
+      val input = new Array[Input](n)
       for (j <- 0 until n) {
-        list(j) = sc.nextInt
+         input(j) = sc.next match {
+           case "get" => Get(sc.next)
+           case "put" => Put(sc.next, sc.next)
+         }
+         
       }
-      cases(i) = list.toList
+      cases(i) = l -> input.toList
 
     }
-
     cases.toList
+  }
+  
+  class LruCache[K, T](val limit: Limit) {
+    private val queue = new mutable.LinkedHashMap[K, T]()
 
+    def get(k: K): Option[T] = ???
+
+    def put(k: K, v: T): Option[K] = ???
   }
 
-  class Queue[T] {
-    class Node(val value: T,
-               var prev: Option[Item] = None,
-               var next: Option[Item] = None)
-
-    private var front: Option[Item] = None
-    private var rear: Option[Item] = None
-
-    def isEmpty = front.isEmpty && rear.isEmpty
-
-    def update(node: Node) = ???
-
-    def enqueue(v: T) = {
-      val n = new Node(v, None, rear)
-      if (isEmpty) {
-        front = rear = Some(n)
-      } else {
-        rear.prev = Some(n)
-        rear = n
-      }
-    }
-
-    def expire() = {
-      if (isEmpty) {
-        front = rear = None
-      } else {
-        front = front.prev
-        if (front.isEmpty) front = rear
-      }
-    }
-
-  }
-
-  class LruCache[K, T](val limit: Int) {
-    private val queue = new Queue[T]()
-    private val map = new mutable.Map[K, Queue[T]#Node]
-
-    def get(k: K): Option[T] = {
-      val res = map.get(k)
-      res match {
-        case Some(_) =>
-          queue.update(k)
-          res
-        case _ =>
-          res
-      }
-    }
-
-    def put(k: K, v: T): Option[K] = {
-      if (limit < map.size) {
-        val node = queue.enqueue(v)
-        map += (k -> node)
-      } else {
-        val exp = queue.expire
-        map.remove(exp.key)
-      }
-    }
-  }
-
-  case class Solution(numbers: List[Int]) {
-    private val cache = new LruCache[Int]()
-    final def solve(): Int = {
-      0
-    }
+  case class Solution(limit: Limit, input: List[Input]) {
+    private val cache = new LruCache[Int, String](limit)
+    final def solve(): List[Int] = Nil
 
   }
 
   def main(args: Array[String]) = {
     val cases = readInput()
 
-    cases.foreach { c =>
-      println(Solution(c).solve())
+    cases.foreach { case (l, actions) =>
+      println(Solution(l, actions).solve())
     }
   }
 }
