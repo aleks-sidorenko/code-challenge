c292cf8ff7a436f9753c58413d82d6e15d915a8a
Alexander Sidorenko <aleks.sidorenko@gmail.com>
2019-03-29 15:33:13 +0200

    [Diameter of Binary Tree] Done + backtracking

--- challenge/geeksforgeeks/diameter-of-binary-tree/main.scala
+++ challenge/geeksforgeeks/diameter-of-binary-tree/main.scala
@@ -9,7 +9,7 @@ object Solution {
     def right: Option[Tree[T]]
   }
 
-  case class Node[T](data: T, right: Option[Tree[T]], left: Option[Tree[T]]) extends Tree[T]
+  case class Node[T](data: T, left: Option[Tree[T]], right: Option[Tree[T]]) extends Tree[T]
 
   case class Leaf[T](data: T) extends Tree[T] {
     val left = None
@@ -18,12 +18,15 @@ object Solution {
   
   type Case = Tree[Data]
   type Diameter = Int
+  type Depth = Int
     
 
   def readInput(): List[Case] = {
     val tree1: Tree[Int] = Node(1, Some(Leaf(2)), Some(Leaf(3)))
     val tree2: Tree[Int] = Node(4, Some(tree1), Some(Leaf(6)))
-    tree1 :: tree2 :: Nil
+    val tree3: Tree[Int] = Node(7, Some(tree2), Some(Leaf(8)))
+    val tree4: Tree[Int] = Leaf(1)
+    tree1 :: tree2 :: tree3 :: tree4 :: Nil
   }
 
   sealed trait Solution {
@@ -36,18 +39,32 @@ object Solution {
     final def solve(input: Case): Diameter = {
       val tree = input
 
-      def diameter(tree: Option[Tree[Data]], current: Diameter): Diameter = {
-        tree match {
-          case Some(t) => 
-            math.max(
-              diameter(t.left, 0) + diameter(t.right, 0),
-              diameter(t.left, current + 1) + diameter(t.right, current + 1)
-            )
-          case None => current
+      val cache = collection.mutable.HashMap.empty[Data, Depth]
+
+      def depth(node: Option[Tree[Data]], acc: Depth): Depth = {        
+        node match {
+          case Some(n) =>
+            cache.getOrElseUpdate(n.data, math.max(depth(n.left, acc + 1), depth(n.right, acc + 1)))
+          case _ => 
+            acc - 1
         }
       }
 
-      diameter(Some(tree), 0)
+      def diameter(tree: Option[Tree[Data]], root: Tree[Data], d: Diameter): Diameter = {        
+        tree match {
+          case Some(t) if t == root => 
+            math.max(
+              depth(t.left, 0) + depth(t.right, 0) + 2,
+              math.max(diameter(t.left, t, 0), diameter(t.right, t, 0))
+              )
+          case Some(t) => 
+            d + 1 + math.max(depth(t.left, 0), depth(t.right, 0))
+          case None => 
+            d
+        }
+      }
+
+      diameter(Some(tree), tree, 0)
     
     }
   }
