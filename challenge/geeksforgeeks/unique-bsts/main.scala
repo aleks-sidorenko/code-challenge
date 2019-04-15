ffffb216ae9e6ab3043a10e659838c1fb445b942
Alexander Sidorenko <aleks.sidorenko@gmail.com>
2019-04-15 19:19:37 +0200

    [Unique BST's] Solved

--- challenge/geeksforgeeks/unique-bsts/main.scala
+++ challenge/geeksforgeeks/unique-bsts/main.scala
@@ -2,21 +2,20 @@ object Solution {
   import scala.collection._
   
 
-  def readInput(): List[Int] = {
-    2 :: 3  :: Nil
+  def input(): List[Int] = {
+    2 :: 3  :: 4 :: Nil
   }
 
-  def solution(t: Int): Int = {
-    t
+  def solve(n: Int): Int = {
+    if (n <= 1) 1
+    else (1 to n).map(i => solve(i - 1) * solve(n - i)).sum
   }
 
   
-
   def main(args: Array[String]) = {
-    val cases = readInput()
-   
+    val cases = input() 
     cases.foreach { c =>
-      println(s"${solution(c)}")
+      println(s"${solve(c)}")
     }
   }
 }
