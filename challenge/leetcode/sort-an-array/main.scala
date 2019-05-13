5d21e8ee43c02816024e62d384985486b0c1539c
Alexander Sidorenko <aleks.sidorenko@gmail.com>
2019-05-13 22:57:38 +0300

    [Swap Nodes in Pairs] Done

--- challenge/leetcode/sort-an-array/main.scala
+++ challenge/leetcode/sort-an-array/main.scala
@@ -1,29 +1,8 @@
 object Solution {
+    def sortArray(nums: Array[Int]): Array[Int] = {
+
+        val cache = nums.groupBy(identity).mapValues(_.length)
         
-    def longestPalindrome(s: String): String = {
-        
-        val chars = s.toArray
-        val cache = Array.ofDim[Boolean](s.length, s.length)
-                
-        @inline
-        def isPalindrom(from: Int, to: Int): Boolean = {
-            cache(from)(to) = chars(from) == chars(to) && (to - from < 3 || cache(from + 1)(to - 1)) 
-            cache(from)(to)
-        } 
-        
-        var from = 0
-        var to = -1
-        
-        for {
-            f <- s.length - 1 to 0 by -1
-            t <- f until s.length
-        } { 
-            if (isPalindrom(f, t) && to - from < t - f) {
-                to = t
-                from = f
-            }
-        }
-                        
-        s.substring(from, to + 1)
+        (-50000 to 50000).filter(cache.contains(_)).flatMap { x =>  List.fill(cache(x))(x) } toArray
     }
 }
