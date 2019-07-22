ad2dbeeaf801c92b59981ebc01a64dd7e27dd479
Alexander Sidorenko <aleks.sidorenko@gmail.com>
2019-07-22 23:11:39 +0300

    [Difference between sums of odd level and even level nodes of BT] Done

--- challenge/geeksforgeeks/difference-between-sums-of-odd-and-even-levels/main.scala
+++ challenge/geeksforgeeks/difference-between-sums-of-odd-and-even-levels/main.scala
@@ -4,12 +4,19 @@ object Solution {
   case class Node(v: Int, left: Option[Node] = None, right: Option[Node] = None)
 
   def input(): List[Node]= {
-    Node(1, Node(2), Node(3)) :: Nil
+    Node(1, Some(Node(2)), Some(Node(3))) :: Nil
   }
 
   def solve(tree: Node): Int = {  
-    0
+    def bfs(n: Node, level: Int): List[(Int, Int)] = (level, n.v) :: n.left.map(bfs(_, level + 1)).getOrElse(Nil) ++ n.right.map(bfs(_, level + 1)).getOrElse(Nil)
+
+    val traversal = bfs(tree, 1)
+    val (odd, even) = traversal.foldLeft(0 -> 0) { case ((odd, even), (lvl, v)) => 
+      if (lvl % 2 == 0) (odd, even + v) else (odd + v, even)
+    }
     
+    odd - even
+
   }
 
   
