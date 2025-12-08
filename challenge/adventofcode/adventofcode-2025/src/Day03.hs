256c6d3d3e57e1085f8ac5a66ff1647961005fb7
Alexander Sidorenko <aleks.sidorenko@gmail.com>
2025-12-08 17:37:27 +0200

    [aoc] day 3, part 2

--- challenge/adventofcode/adventofcode-2025/src/Day03.hs
+++ challenge/adventofcode/adventofcode-2025/src/Day03.hs
@@ -2,10 +2,13 @@
 module Day03
   ( parseInput
   , solvePart1
+  , solvePart2
   , solve
-  , largestJoltage
+  , largestJoltage1
+  , largestJoltage2
   ) where
 
+type Digit = Int
 type Joltage = Int
 type Battery = [Joltage]
 type Input = [Battery]
@@ -19,8 +22,8 @@ parseInput = map parseBattery . lines where
   parseBattery = map parseJoltage
     
 
-largestJoltage :: Battery -> Joltage
-largestJoltage xs = 
+largestJoltage1 :: Battery -> Joltage
+largestJoltage1 xs = 
   let allExceptLast = init xs
       maxVal = maximum allExceptLast
       maxIndex = firstIndexOf maxVal allExceptLast
@@ -32,13 +35,32 @@ largestJoltage xs =
     firstIndexOf x xs = 
       let indices = [i | (i, v) <- zip [0..] xs, v == x]
       in head indices 
-  
-  
+
+largestJoltage2 :: Int -> Battery -> Joltage
+largestJoltage2 n xs = digitsToJoltage $ reverse $ loop n xs []
+  where
+    loop :: Int -> Battery -> [Digit] -> [Digit]
+    loop n xs result =
+      if n == 0 || null xs then result
+      else  loop (n - 1) xs_ result_
+      where
+        max = maximum (take (length xs - n + 1) xs)
+        maxIndex = firstIndexOf max xs
+        result_ = max : result
+        xs_ = drop (maxIndex + 1) xs
+        firstIndexOf :: Eq a => a -> [a] -> Int
+        firstIndexOf x xs = 
+          let indices = [i | (i, v) <- zip [0..] xs, v == x]
+          in head indices
+    digitsToJoltage :: [Digit] -> Joltage
+    digitsToJoltage = foldl (\acc x -> acc * 10 + x) 0
+    
+    
 solvePart1 :: Input -> Int
-solvePart1 = sum . map largestJoltage
+solvePart1 = sum . map largestJoltage1
 
 solvePart2 :: Input -> Int
-solvePart2 = undefined
+solvePart2 = sum . map (largestJoltage2 12)
 
 
 -- Main solve function - reads input and prints results
@@ -48,4 +70,4 @@ solve = do
   let input = parseInput content
   putStrLn $ "Day 2 Solutions:"
   putStrLn $ "Part 1: " ++ show (solvePart1 input)
-  -- putStrLn $ "Part 2: " ++ show (solvePart2 input)
+  putStrLn $ "Part 2: " ++ show (solvePart2 input)
--- challenge/adventofcode/adventofcode-2025/test/Day03Spec.hs
+++ challenge/adventofcode/adventofcode-2025/test/Day03Spec.hs
@@ -11,10 +11,17 @@ spec = do
         let input = "811111111111119\n234234234234278\n818181911112111"
         let expected = [[8,1,1,1,1,1,1,1,1,1,1,1,1,1,9], [2,3,4,2,3,4,2,3,4,2,3,4,2,7,8], [8,1,8,1,8,1,9,1,1,1,1,2,1,1,1]]
         parseInput input `shouldBe` expected
-    describe "largestJoltage" $ do
+    describe "largestJoltage1" $ do
       it "returns the largest joltage in a battery" $ do
-        largestJoltage [8,1,1,1,1,1,1,1,1,1,1,1,1,1,9] `shouldBe` 89
-        largestJoltage [2,3,4,2,3,4,2,3,4,2,3,4,2,7,8] `shouldBe` 78
-        largestJoltage [8,1,8,1,8,1,9,1,1,1,1,2,1,1,1] `shouldBe` 92
+        largestJoltage1 [8,1,1,1,1,1,1,1,1,1,1,1,1,1,9] `shouldBe` 89
+        largestJoltage1 [2,3,4,2,3,4,2,3,4,2,3,4,2,7,8] `shouldBe` 78
+        largestJoltage1 [8,1,8,1,8,1,9,1,1,1,1,2,1,1,1] `shouldBe` 92
+
+    describe "largestJoltage2" $ do
+      it "returns the largest joltage in a battery" $ do
+        largestJoltage2 12 [9,8,7,6,5,4,3,2,1,1,1,1,1,1,1,1] `shouldBe` 987654321111
+        largestJoltage2 12 [8,1,1,1,1,1,1,1,1,1,1,1,1,1,9] `shouldBe` 811111111119
+        largestJoltage2 12 [2,3,4,2,3,4,2,3,4,2,3,4,2,7,8] `shouldBe` 434234234278
+        largestJoltage2 12 [8,1,8,1,8,1,9,1,1,1,1,2,1,1,1] `shouldBe` 888911112111
 
     
